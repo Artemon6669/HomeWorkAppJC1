03bfa0c46c2eacc561689caaae935a800e3eaeb9
Artem Nebogatikov <artemon6669@gmail.com>
2022-03-16 16:17:46 +0400

    lession5

---
+++ src/Lession5/AppData.java
@@ -0,0 +1,14 @@
+package Lession5;
+
+public class AppData {
+    private String[] header;
+    private int[][] data;
+
+    public void setData(int[][] data) {
+        this.data = data;
+    }
+
+    public void setHeader(String[] header) {
+        this.header = header;
+    }
+}
